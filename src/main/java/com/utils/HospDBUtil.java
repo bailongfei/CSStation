a74b1d7b5f1d6dc61e690dfb12948404fa6c8a00
laoShiRen <36829795+laoShiRen1207@users.noreply.github.com>
2018-12-03 15:35:57 +0800

    保护隐私

--- src/main/java/com/utils/HospDBUtil.java
+++ src/main/java/com/utils/HospDBUtil.java
@@ -10,9 +10,9 @@ public class HospDBUtil {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        String url = "jdbc:oracle:thin:@128.100.200.86:1521:HISDB1";
-        String userName = "HU_INF";
-        String password = "HUINF";
+        String url = "****";
+        String userName = "***";
+        String password = "***";
         return DriverManager.getConnection(url, userName, password);
     }
 
